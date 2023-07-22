d7c5ce5362b08a8e08accb87a5d3a4b28156687c
glaucc <ahmedovyusif8785@gmail.com>
2023-07-22 20:29:34 +0400

    configs

---
+++ project-cyber/styles/themes/darkTheme.js
@@ -0,0 +1,8 @@
+// styles/themes/darkTheme.js
+const darkTheme = {
+    background: '#1a1a1a',
+    text: '#ffffff',
+  };
+  
+  export default darkTheme;
+  
---
+++ project-cyber/styles/themes/lightTheme.js
@@ -0,0 +1,8 @@
+// styles/themes/lightTheme.js
+const lightTheme = {
+    background: '#ffffff',
+    text: '#333333',
+  };
+  
+  export default lightTheme;
+  
