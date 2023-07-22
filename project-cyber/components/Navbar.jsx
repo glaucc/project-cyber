d50aea5b11cfc24c0f1051cec2b90655700150d7
glaucc <ahmedovyusif8785@gmail.com>
2023-07-22 20:33:57 +0400

    dark mode

--- project-cyber/components/Navbar.jsx
+++ project-cyber/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+
+const lightTheme = {
+  backgroundColor: "#f8f8f8",
+  textColor: "#333",
+  linkHoverColor: "#555",
+};
+
+const darkTheme = {
+  backgroundColor: "#222",
+  textColor: "#f8f8f8",
+  linkHoverColor: "#ddd",
+};
 
 const NavbarContainer = styled.div`
-  background-color: #f8f8f8; /* Light background color */
+  background-color: ${(props) => props.theme.backgroundColor};
   padding: 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  cursor: default; /* Change the default cursor to the default arrow */
+  cursor: default;
 
   @media (max-width: 768px) {
     flex-direction: column;
@@ -16,7 +28,7 @@ const NavbarContainer = styled.div`
   }
 
   @media (max-width: 425px) {
-    max-width:100vw;
+    max-width: 100vw;
   }
 `;
 
@@ -29,44 +41,55 @@ const NavItems = styled.div`
 `;
 
 const NavItem = styled.a`
-  color: #333; /* Light text color */
+  color: ${(props) => props.theme.textColor};
   font-size: 16px;
   margin: 0 10px;
   text-decoration: none;
   padding: 10px;
   border-radius: 5px;
   transition: background-color 0.3s ease;
-  cursor: pointer; /* Change the cursor to a pointer on hover */
+  cursor: pointer;
 
   &:hover {
-    background-color: #555;
+    background-color: ${(props) => props.theme.linkHoverColor};
   }
 `;
 
 const Navbar = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
   return (
-    <NavbarContainer>
-      <Link href="/">
-        <NavItem>Home</NavItem>
-      </Link>
-      <NavItems className="navitems">
-        <Link href="/learn">
-          <NavItem className="navitemss">Learn</NavItem>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <NavbarContainer>
+        <Link href="/">
+          <NavItem>Home</NavItem>
         </Link>
-        <Link href="/practice">
-          <NavItem className="navitemss">Practice</NavItem>
-        </Link>
-        <Link href="/paths">
-          <NavItem className="navitemss">Paths</NavItem>
-        </Link>
-        <Link href="/login">
-          <NavItem className="navitemss">Login</NavItem>
-        </Link>
-        <Link href="/signup">
-          <NavItem className="navitemss">Sign Up</NavItem>
-        </Link>
-      </NavItems>
-    </NavbarContainer>
+        <NavItems className="navitems">
+          <Link href="/learn">
+            <NavItem className="navitemss">Learn</NavItem>
+          </Link>
+          <Link href="/practice">
+            <NavItem className="navitemss">Practice</NavItem>
+          </Link>
+          <Link href="/paths">
+            <NavItem className="navitemss">Paths</NavItem>
+          </Link>
+          <Link href="/login">
+            <NavItem className="navitemss">Login</NavItem>
+          </Link>
+          <Link href="/signup">
+            <NavItem className="navitemss">Sign Up</NavItem>
+          </Link>
+          <NavItem className="navitemss" onClick={toggleDarkMode}>
+            {isDarkMode ? "Light Mode" : "Dark Mode"}
+          </NavItem>
+        </NavItems>
+      </NavbarContainer>
+    </ThemeProvider>
   );
 };
 
