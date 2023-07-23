7cba8d567f1a1d69ac0f7ea3cfab7a5a001c7b20
glaucc <ahmedovyusif8785@gmail.com>
2023-07-23 20:12:27 +0400

    fix

--- project-cyber/app/layout.js
+++ project-cyber/app/layout.js
@@ -11,9 +11,45 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import { ThemeProvider, useTheme, darkTheme } from "../components/ThemeContext";
 
 
+
+
+  
+const ContentContainer = styled.div`
+max-width: 1200px;
+margin: 0 auto;
+padding: 20px;
+`;
+
+
+
+
+// background-color: ${(props) => props.theme.backgroundColor};
+// color: ${(props) => props.theme.textColor};
+
+// }
+
+// a {
+// color: ${(props) => props.theme.textColor};
+// text-decoration: none;
+// }
+
+// a:hover {
+// color: ${(props) => props.theme.linkHoverColor};
+// }
+// const inter = Inter({ subsets: ['latin'] })
+// className={inter.className}
+
+
+
+
+const RootLayout = ({ children }) => {
+  console.log(2)
+
+
+  const { theme } = useTheme();
 // Create a GlobalStyle component to apply the Inter font globally
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
 
   /* Other global styles or CSS resets can go here */
 
@@ -22,35 +58,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Inter', sans-serif;
   
-  }
-
-  
   `;
-  // a {
-  //   color: ${(props) => props.theme.textColor};
-  //   text-decoration: none;
-  // }
 
-  // a:hover {
-  //   color: ${(props) => props.theme.linkHoverColor};
-  // }
-  // background-color: ${(props) => props.theme.backgroundColor};
-  // color: ${(props) => props.theme.textColor};
 
-  
-const ContentContainer = styled.div`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px;
-`;
-
-// const inter = Inter({ subsets: ['latin'] })
-// className={inter.className}
-
-const RootLayout = ({ children }) => {
-  console.log(2)
-
-  const { theme } = useTheme();
   console.log(3)
 
   return (
